a9039ed5322911f2dd9f4dbecdf806b7dfc6fb16
EmilSuuronen <emilsuuronen@gmail.com>
2024-04-23 14:44:09 +0300

    add initial structure

---
+++ server/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
