ff0dbe00ed080fc58845ab3c5291e00e40a7624b
EmilSuuronen <emilsuuronen@gmail.com>
2024-04-23 15:56:03 +0300

    change configuration file structure

--- server/app.ts
+++ server/app.ts
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
--- server/dist/app.js
+++ server/dist/app.js
@@ -7,10 +7,10 @@ const express_1 = __importDefault(require("express"));
 const path = require('path');
 const app = (0, express_1.default)();
 // Serve static files from the React app
-app.use(express_1.default.static(path.join(__dirname, '../client/build')));
+app.use(express_1.default.static(path.join(__dirname, '../../client/build')));
 // Handle React routing, return all requests to React app
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
