21f22a55d94620f41a247c49b35d7cca18be192b
EmilSuuronen <emilsuuronen@gmail.com>
2024-04-23 15:21:13 +0300

    nodejs to serve react frontend

---
+++ server/dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path = require('path');
+const app = (0, express_1.default)();
+// Serve static files from the React app
+app.use(express_1.default.static(path.join(__dirname, '../client/build')));
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+//# sourceMappingURL=app.js.map
