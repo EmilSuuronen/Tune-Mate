91fc8a89c99164c9812f4941fdc5b1707de4aeaf
EmilSuuronen <emilsuuronen@gmail.com>
2024-04-24 20:47:37 +0300

    add createUser, fix errors

--- server/src/api/resolvers/UserResolver.ts
+++ server/src/api/resolvers/UserResolver.ts
@@ -1,106 +1,37 @@
 import {GraphQLError} from 'graphql';
-import {LoginUser, User, UserInput} from '../../types/typeDefs';
-import fetchData from '../../functions/fetchData';
-import {LoginResponse, UserResponse} from '../../types/MessageTypes';
-import {MyContext} from '../../types/MyContext';
+import {User, UserInput} from '../../types/typeDefs';
 import userModel from "../models/userModel";
 
+
 export default {
-  Query: {
-    users: async () => {
-      return userModel.find();
+    Query: {
+        users: async () => {
+            return userModel.find();
+        },
+        user: async (
+            _parent: undefined,
+            args: { id: string }
+        ) => {
+            const user = await userModel.findById(args.id);
+            if (!user) {
+                throw new GraphQLError('User not found');
+            }
+            return user;
+        }
     },
-    userById: async (_parent: undefined, args: {id: string}) => {
-        return userModel.findById(args.id);
-    },
-    checkToken: async (
-      _parent: undefined,
-      _args: undefined,
-      context: MyContext,
-    ) => {
-      const response = {
-        message: 'Token is valid',
-        user: context.userdata,
-      };
-      return response;
-    },
-  },
-  Mutation: {
-    createUser: async (
-        _parent: undefined,
-        args: {username: String, email: String, password: String},
+
+    Mutation: {
+        createUser: async (
+            _parent: undefined,
+            args: { input: UserInput },
         ): Promise<User> => {
-      return userModel.create({
-          user_name: args.username,
-          email: args.email,
-          password: args.password,
-        });
-    },
-    login: async (
-      _parent: undefined,
-      args: {credentials: {email: string; password: string}},
-    ): Promise<LoginResponse & {token: string; user: LoginUser}> => {
-      if (!process.env.AUTH_URL) {
-        throw new GraphQLError('No auth url set in .env file');
-      }
-      const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(args.credentials),
-      };
-      const loginResponse = await fetchData<
-        LoginResponse & {token: string; user: LoginUser}
-      >(process.env.AUTH_URL + '/auth/login', options);
-      loginResponse.user.id = loginResponse.user._id;
-      return loginResponse;
-    },
-    updateUser: async (
-      _parent: undefined,
-      args: {user: UserInput},
-      context: MyContext,
-    ): Promise<UserResponse> => {
-      if (!context.userdata) {
-        throw new GraphQLError('User not authenticated', {
-          extensions: {code: 'UNAUTHENTICATED'},
-        });
-      }
-      const options = {
-        method: 'PUT',
-        headers: {
-          'CONTENT-TYPE': 'application/json',
-          Authorization: 'Bearer ' + context.userdata.token,
-        },
-        body: JSON.stringify(args.user),
-      };
-      const user = await fetchData<UserResponse>(
-        process.env.AUTH_URL + '/users',
-        options,
-      );
-      return user;
-    },
-    deleteUser: async (
-      _parent: undefined,
-      _args: undefined,
-      context: MyContext,
-    ): Promise<UserResponse> => {
-      if (!context.userdata) {
-        throw new GraphQLError('User not authenticated', {
-          extensions: {code: 'UNAUTHENTICATED'},
-        });
-      }
-      const options = {
-        method: 'DELETE',
-        headers: {
-          Authorization: 'Bearer ' + context.userdata.token,
-        },
-      };
-      const user = await fetchData<UserResponse>(
-        process.env.AUTH_URL + '/users',
-        options,
-      );
-      return user;
-    },
-  },
+            return await userModel.create(
+                {
+                    user_name: args.input.user_name,
+                    email: args.input.email,
+                    password: args.input.password
+                }
+            );
+        }
+    }
 };
