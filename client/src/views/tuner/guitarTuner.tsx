08c4a64b5c539f26e6effcc3782a50a5e4cbc088
EmilSuuronen <emilsuuronen@gmail.com>
2024-05-07 19:06:09 +0300

    add own tunings to guitarTuner

--- client/src/views/tuner/guitarTuner.tsx
+++ client/src/views/tuner/guitarTuner.tsx
@@ -2,21 +2,105 @@ import React, { useState, useEffect } from 'react';
 import { PitchDetector } from "pitchy";
 import "./guitarTuner.css"
 import SideNav from "../../components/sidenav/sidenav";
+import {useQuery} from "@apollo/client";
+import {FIND_TUNING_BY_USER} from "../graphql/tuningTypes";
+import Tuning from "../tuning/TuningInterface";
 
 const GuitarTuner: React.FC = () => {
     const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
     const [frequency, setFrequency] = useState<number>(40);
     const [isListening, setIsListening] = useState<boolean>(false);
     const [targetFrequency, setTargetFrequency] = useState<number>(42);
+    const [tuningsByUser, setTuningsByUser] = useState<Tuning[]>([]);
+    const [formattedTunings, setFormattedTunings] = useState<TuningFormatted[]>([]);
+    const userId = localStorage.getItem("currentUser");
+
+    const {loading: loadingTunings, data: tuningsData} = useQuery(FIND_TUNING_BY_USER, {
+        variables: {input: {id: userId}}
+    });
+
+    useEffect(() => {
+        if (tuningsData && tuningsData.findTuningsByOwner) {
+            setTuningsByUser(tuningsData.findTuningsByOwner);
+            console.log("tunings by user: ", tuningsByUser);
+        }
+    }, [tuningsData, tuningsByUser]);
 
     const strings = [
-        { name: 'E Low', freq: 82.41 },
+        { name: 'E', freq: 82.41 },
         { name: 'A', freq: 110.00 },
         { name: 'D', freq: 146.83 },
         { name: 'G', freq: 196.00 },
         { name: 'B', freq: 246.94 },
-        { name: 'e High', freq: 329.63 }
+        { name: 'e', freq: 329.63 }
     ];
+
+    type NoteFrequencies = {
+        "D#": number;
+        "E": number;
+        "F": number;
+        "F#": number;
+        "G": number;
+        "G#": number;
+        "A": number;
+        "A#": number;
+        "C": number;
+        "C#": number;
+        "D": number;
+        "B": number;
+        "E#": number;
+        "e": number;
+    };
+
+    interface Tuning {
+        name: string;
+        string_count: number;
+        string_notes: (keyof NoteFrequencies)[];
+        owner: string;
+    }
+
+    const noteFrequencies: NoteFrequencies = {
+        "D#": 155.56,
+        "E": 164.81,
+        "F": 174.61,
+        "F#": 185.00,
+        "G": 196.00,
+        "G#": 207.65,
+        "A": 110.00,
+        "A#": 116.54,
+        "C": 130.81,
+        "C#": 138.59,
+        "D": 146.83,
+        "B": 246.94,
+        "E#": 174.61,
+        "e": 329.63
+    };
+
+    function mapTuningsToFrequency(tunings: Tuning[]) {
+        return tunings.map(tuning => ({
+            name: tuning.name,
+            strings: tuning.string_notes.map(note => ({
+                name: note,
+                freq: noteFrequencies[note] || 0 // Safe to access because `note` is a keyof NoteFrequencies
+            }))
+        }));
+    }
+
+    type NoteFrequencyMapping = {
+        name: string;
+        freq: number;
+    };
+
+    type TuningFormatted = {
+        name: string;
+        strings: NoteFrequencyMapping[];
+    };
+
+    useEffect(() => {
+        const formatted = mapTuningsToFrequency(tuningsByUser);
+        setFormattedTunings(formatted);
+        console.log("formattedTunings: ", formattedTunings);
+    }, [tuningsByUser]);
 
     useEffect(() => {
         if (isListening) {
@@ -81,6 +165,8 @@ const GuitarTuner: React.FC = () => {
         frequencies.push(i);
     }
 
+    if (loadingTunings) return <p>Loading...</p>;
+
     return (
         <div className="div-tuner-main">
             <SideNav/>
@@ -98,7 +184,7 @@ const GuitarTuner: React.FC = () => {
                     ))}
                 </div>
                 <select className="selector-guitar-tuner-string" onChange={handleStringChange} value={targetFrequency.toString()}>
-                    <option value="40">Select String to Tune</option>
+                    <option value="40">Ready Tunings</option>
                     <optgroup label="Standard Tuning">
                         {strings.map(string => (
                             <option key={string.name} value={string.freq.toString()}>
@@ -106,6 +192,18 @@ const GuitarTuner: React.FC = () => {
                             </option>
                         ))}
                     </optgroup>
+                </select>
+                <select className="selector-guitar-tuner-string" name="tunings" id="tuning-select" onChange={handleStringChange}>
+                    <option value="40">Your tunings</option>
+                    {formattedTunings.map(tuning => (
+                        <optgroup key={tuning.name} label={tuning.name}>
+                            {tuning.strings.map(string => (
+                                <option key={`${tuning.name}-${string.name}`} value={string.freq.toString()}>
+                                    {string.name} - {string.freq} Hz
+                                </option>
+                            ))}
+                        </optgroup>
+                    ))}
                 </select>
                 <p>Tune Frequency: <span style={{ color: isCloseToTarget ? 'green' : 'red' }}>{frequency.toFixed(2)} Hz</span></p>
                 <button className="button-color" onClick={() => setIsListening(!isListening)}>
